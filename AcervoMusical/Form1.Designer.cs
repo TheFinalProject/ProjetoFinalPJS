b6ee0707459fc633d1632a58268a8e1e2c2d4f9e
Luis Gustavo Ferreira Lucio <luisinhufut@hotmail.com>
2012-11-01 21:26:35 -0200

    Tela de Login

--- AcervoMusical/Form1.Designer.cs
+++ AcervoMusical/Form1.Designer.cs
@@ -28,12 +28,88 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBox_Login = new System.Windows.Forms.TextBox();
+            this.button_Entrar = new System.Windows.Forms.Button();
+            this.textBox_Senha = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button_Cancelar = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox_Login
+            // 
+            this.textBox_Login.ForeColor = System.Drawing.Color.DarkGray;
+            this.textBox_Login.Location = new System.Drawing.Point(6, 19);
+            this.textBox_Login.Name = "textBox_Login";
+            this.textBox_Login.Size = new System.Drawing.Size(174, 20);
+            this.textBox_Login.TabIndex = 1;
+            this.textBox_Login.Text = "Nome";
+            // 
+            // button_Entrar
+            // 
+            this.button_Entrar.Location = new System.Drawing.Point(21, 220);
+            this.button_Entrar.Name = "button_Entrar";
+            this.button_Entrar.Size = new System.Drawing.Size(75, 23);
+            this.button_Entrar.TabIndex = 2;
+            this.button_Entrar.Text = "Entrar";
+            this.button_Entrar.UseVisualStyleBackColor = true;
+            // 
+            // textBox_Senha
+            // 
+            this.textBox_Senha.ForeColor = System.Drawing.Color.DarkGray;
+            this.textBox_Senha.Location = new System.Drawing.Point(6, 45);
+            this.textBox_Senha.Name = "textBox_Senha";
+            this.textBox_Senha.Size = new System.Drawing.Size(174, 20);
+            this.textBox_Senha.TabIndex = 4;
+            this.textBox_Senha.Text = "Senha";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox_Login);
+            this.groupBox1.Controls.Add(this.textBox_Senha);
+            this.groupBox1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.groupBox1.Location = new System.Drawing.Point(12, 128);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(198, 86);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Login";
+            // 
+            // button_Cancelar
+            // 
+            this.button_Cancelar.Location = new System.Drawing.Point(120, 220);
+            this.button_Cancelar.Name = "button_Cancelar";
+            this.button_Cancelar.Size = new System.Drawing.Size(75, 23);
+            this.button_Cancelar.TabIndex = 6;
+            this.button_Cancelar.Text = "Cancelar";
+            this.button_Cancelar.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(301, 268);
+            this.Controls.Add(this.button_Cancelar);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button_Entrar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox_Login;
+        private System.Windows.Forms.Button button_Entrar;
+        private System.Windows.Forms.TextBox textBox_Senha;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button_Cancelar;
     }
 }
 
